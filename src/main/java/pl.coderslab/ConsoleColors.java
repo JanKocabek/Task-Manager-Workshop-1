b30789bc235655988808abb6e5d6bfcc0b57ae91
Jan Kocábek <jankocabek93@gmail.com>
2024-12-19 16:58:01 +0100

    Initial setup with maven and dependencies

--- src/main/java/pl.coderslab/ConsoleColors.java
+++ src/main/java/pl.coderslab/ConsoleColors.java
@@ -1,6 +1,5 @@
 package pl.coderslab;
 
-
 /**
  * https://stackoverflow.com/a/45444716
  */
@@ -77,4 +76,5 @@ public class ConsoleColors {
     public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
     public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
     public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
+
 }
